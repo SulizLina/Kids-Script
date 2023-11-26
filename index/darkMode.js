25c5142839473e9e92eb0dcac07295fb8dbe497e
SulizLina <alzeghaibilina@gmail.com>
2023-11-26 22:50:36 +0300

    Create darkMode.js

---
+++ index/darkMode.js
@@ -0,0 +1,56 @@
+ 
+ 
+ /*
+  var icon = document. getElementById ("icon");
+  icon.onclick = function () {
+  document. body.classList. toggle ("dark-theme");
+  }
+ 
+  var icon = document. getElementById ("icon");
+  icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if(document.body.classList.contains("dark-theme")){
+   icon.src = "images/sun.png"; 
+  }
+  else{
+  icon.src = "images/moon.png";
+  }
+}
+   */
+
+ 
+function toggleDarkTheme() {
+  var body = document.body;
+  body.classList.toggle("dark-theme");
+
+ 
+  var icon = document.getElementById("icon");
+  if (body.classList.contains("dark-theme")) {
+    icon.src = "images/sun.png";
+  } else {
+    icon.src = "images/moon.png";
+  }
+
+ 
+  var isDarkThemeActive = body.classList.contains("dark-theme");
+  localStorage.setItem("isDarkThemeActive", isDarkThemeActive.toString());
+}
+
+ 
+document.addEventListener("DOMContentLoaded", function () {
+  var isDarkThemeActive = localStorage.getItem("isDarkThemeActive");
+  var body = document.body;
+  var icon = document.getElementById("icon");
+
+  if (isDarkThemeActive === "true") {
+    body.classList.add("dark-theme");
+    icon.src = "images/sun.png";
+  } else {
+    body.classList.remove("dark-theme");
+    icon.src = "images/moon.png";
+  }
+});
+
+ 
+var icon = document.getElementById("icon");
+icon.onclick = toggleDarkTheme;
